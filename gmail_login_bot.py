f4f064d1e3f34773575cd250a928ed136fd878c4
Akash <akashsg55@gmail.com>
2019-10-03 14:39:28 +0530

    Version 1.1

--- gmail_login_bot.py
+++ gmail_login_bot.py
@@ -3,19 +3,23 @@
 
 from selenium import webdriver
 import time
+import getpass
 
-username=input("Give the Email address or username= ")
-password=input("Give the password= ")
+def driver(username,password):
+    url='https://gmail.com'
+    driver=webdriver.Firefox()
+    driver.get(url)
+    driver.find_element_by_id('identifierId').send_keys(username)
+    time.sleep(2)
+    driver.find_element_by_id('identifierNext').click()
+    time.sleep(1.5)
+    driver.find_element_by_name('password').send_keys(password)
+    time.sleep(2)
+    driver.find_element_by_id('passwordNext').click()
+    time.sleep(1.5)
+    
+
+u=input("Give the Email address or username= ")
+p = getpass.getpass('Password= ')
 print("Request Processing.............")
-url='https://gmail.com'
-driver=webdriver.Firefox()
-driver.get(url)
-
-driver.find_element_by_id('identifierId').send_keys(username)
-time.sleep(2)
-driver.find_element_by_id('identifierNext').click()
-time.sleep(1.5)
-driver.find_element_by_name('password').send_keys(password)
-time.sleep(2)
-driver.find_element_by_id('passwordNext').click()
-time.sleep(1.5)
+driver(u,p)
